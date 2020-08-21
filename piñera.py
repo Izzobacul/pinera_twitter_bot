c8dadc6386432caf058e388f47cdd8b3a89ae3f9
Izzobacul <izzobacul@gmail.com>
2020-08-20 22:20:09 -0400

    Added repeat-proof

--- piñera.py
+++ piñera.py
@@ -18,8 +18,12 @@ api = twitter.Api(consumer_key=auth['consumer_key'],
                   access_token_key=auth['access_token_key'],
                   access_token_secret=auth['access_token_secret'])
 
+last_id = open('last.txt').read()
 timeline = api.GetUserTimeline(13623532)
 last = timeline[0]
+if last.id_str == last_id:
+    exit()
 stupified = stupify(last.text)
 api.PostUpdate(stupified, in_reply_to_status_id=last.id, auto_populate_reply_metadata=True)
-print(last)
+with open('last.txt', 'w') as out:
+    out.write(last.id_str)
